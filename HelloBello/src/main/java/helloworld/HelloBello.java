52236aa61ca6ebae6c0ff9f654525dab8c081bf0
Zorn <mperneky@gmail.com>
2017-07-09 18:56:42 +0200

    Added two new modules

---
+++ HelloBello/src/main/java/helloworld/HelloBello.java
@@ -0,0 +1,22 @@
+package helloworld;
+
+/**
+ * Created by Yimbo on 2017.07.02..
+ */
+public class HelloBello {
+    private final long id;
+    private final String content;
+
+    public HelloBello(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
---
+++ HelloBello/src/main/java/helloworld/HelloController.java
@@ -0,0 +1,26 @@
+package helloworld;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Created by Yimbo on 2017.07.02..
+ */
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+
+    private static final String template = "Hello-Bello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping(method = RequestMethod.GET)
+    public @ResponseBody HelloBello greet(@RequestParam(value="name", required=false, defaultValue="Bob") String name) {
+        return new HelloBello(counter.incrementAndGet(), String.format(template, name));
+    }
+}
