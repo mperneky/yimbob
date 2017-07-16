e064047dffc5cf71222913b2557da636a170deb2
Zorn <mperneky@gmail.com>
2017-07-16 10:08:49 +0200

    Things on /hello now appear as text, not in the resposne body.

--- HelloBello/src/main/java/helloworld/HelloController.java
+++ HelloBello/src/main/java/helloworld/HelloController.java
@@ -1,17 +1,19 @@
 package helloworld;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Hello controller of my application.
  */
-@Controller
+@RestController
 @RequestMapping("/hello")
 public class HelloController {
 
